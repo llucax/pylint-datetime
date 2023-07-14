1750cd3bbd70cbe09fe3f89ad71144f718c8139c
Jack <jack.herrmann@gmx.de>
2023-07-14 16:35:03 +0200

    Implement custom pylint checker for datetime module
    Signed-off-by: Jack <jack.herrmann@gmx.de>

--- src/pylint_datetime/__init__.py
+++ src/pylint_datetime/__init__.py
@@ -1,4 +1,185 @@
-# License: MIT
-# Copyright © 2023 Frequenz Energy-as-a-Service GmbH
+"""Pylint checker ensure correct usage of datetime module, especially around aware/naive objects."""
 
-"""checker to ensure correct datetime usage with aware objects in datetime module."""
+from typing import Any
+
+import astroid  # type: ignore
+import pylint  # type: ignore
+from pylint.checkers import BaseChecker  # type: ignore
+
+# function calls to fromisoformat, strptime and fromisoformat can't be checked - they parse strings
+# if the specified timezone is None, still allowed although they produce naive objects
+
+
+class DatetimeChecker(BaseChecker):  # type: ignore
+    """class for custom pylint checker."""
+
+    name = "pylint-datetime"
+    priority = -1
+    msgs = {
+        "W9999": (
+            "timedelta() called without keyword arguments",
+            "timedelta-no-keyword-args",
+            "timedelta() called without keyword arguments.",
+        ),
+        "W9998": (
+            'Function call to "%s"/"replace" should be called with a timezone argument',
+            "timezone-no-argument",
+            'Function call to "%s"/"replace" should be called with a timezone argument',
+        ),
+        "W9997": (
+            'Function call to "%s" can only produce naive objects',
+            "naive-datetime-call",
+            'Function call to "%s" can only produce naive objects, aware objects necessary',
+        ),
+        "W9996": (
+            'Attribute access "%s" should be followed by a call to replace with timezone argument',
+            "naive-datetime-no-timezone",
+            'Attribute access "%s" should be followed by a call to replace with timezone argument',
+        ),
+    }
+
+    def __init__(self, linter: pylint.lint.PyLinter = None) -> None:
+        """Initialize checker.
+
+        Args:
+            linter: instance of pylint linter.
+        """
+        super().__init__(linter)
+
+    def timedelta_with_keywords(
+        self, node: astroid.node_classes.NodeNG, name: str
+    ) -> None:
+        """Check calls to timedelta() and datetime.timedelta() for keyword arguments.
+
+        Args:
+            node: the node that visit_call originates from.
+            name: name of the function being called.
+        """
+        if name == "timedelta":
+            for _ in node.args:
+                self.add_message("timedelta-no-keyword-args", node=node)
+            if not node.args and not node.keywords:
+                self.add_message("timedelta-no-keyword-args", node=node)
+
+    # check for positional arguments cheesy, uses fact, that they have one Attribute Argument.
+
+    def timezone_argument(self, node: astroid.node_classes.NodeNG, name: str) -> None:
+        """Check calls to datetime, now, fromtimestamp, astimezone and time for timezone arg.
+
+        Args:
+            node: the node that visit_call originates from.
+            name: name of the function being called.
+        """
+        if name in ("datetime", "now", "fromtimestamp", "astimezone"):
+            if not any(
+                isinstance(arg, astroid.Keyword) and arg.arg in ("tz", "tzinfo")
+                for arg in node.keywords
+            ):
+                if not any(isinstance(arg, astroid.Attribute) for arg in node.args):
+                    self.add_message("timezone-no-argument", node=node, args=(name,))
+
+    def naive_functions(self, node: astroid.node_classes.NodeNG, name: str) -> None:
+        """Check that functions producing only naive objects are never called.
+
+        Args:
+            node: the node that visit_call originates from
+            name: name of the function being called
+        """
+        if name in ("today", "utcnow", "utcfromtimestamp", "utctimetuple", "time"):
+            self.add_message("naive-datetime-call", node=node, args=(name,))
+
+    def visit_call(self, node: astroid.node_classes.NodeNG) -> None:
+        """Pylint function responds when any function is called.
+
+        Args:
+            node: origin node for function call.
+        """
+        if hasattr(node.func, "name"):
+            name = node.func.name
+        elif hasattr(node.func, "attrname"):
+            name = node.func.attrname
+
+        self.timedelta_with_keywords(node, name)
+
+        self.timezone_argument(node, name)
+
+        self.naive_functions(node, name)
+
+    def naive_properties_methods_replace(
+        self, node: astroid.node_classes.NodeNG, assigned_value: Any, assigned_var: Any
+    ) -> None:
+        """Check for a replace call specifiying timezone after naive property or function.
+
+        Args:
+            node: the node that visit_call originates from.
+            assigned_value: the value of the assignment that triggered function call.
+            assigned_var: the variable that the value was assigned to.
+        """
+        next_node = assigned_var
+        next_is_empty = True
+        if next_node and next_node.next_sibling():
+            next_is_empty = False
+            next_node = next_node.next_sibling()
+            if (
+                isinstance(next_node.value, astroid.Call)
+                and isinstance(next_node.value.func, astroid.Attribute)
+                and next_node.value.func.attrname == "replace"
+            ):
+                if not any(
+                    isinstance(arg, astroid.Keyword) and arg.arg == "tzinfo"
+                    for arg in next_node.value.keywords
+                ) and (
+                    not any(
+                        isinstance(arg, astroid.Attribute)
+                        for arg in next_node.value.args
+                    )
+                ):
+                    new_node = next_node.value
+                    self.add_message(
+                        "timezone-no-argument", node=new_node, args=(assigned_value,)
+                    )
+            else:
+                self.add_message(
+                    "naive-datetime-no-timezone", node=node, args=(assigned_value,)
+                )
+        if next_is_empty:
+            self.add_message(
+                "naive-datetime-no-timezone", node=node, args=(assigned_value,)
+            )
+
+    def visit_assign(self, node: astroid.node_classes.NodeNG) -> None:
+        """Pylint function responds when any assignment takes place.
+
+        Args:
+            node: the node that visit_call originates from.
+        """
+        assigned_var = node.targets[0]
+        assigned_value = node.value
+
+        if isinstance(assigned_var, astroid.AssignName):
+            assigned_var_type = assigned_var.inferred()[0]
+            if assigned_var_type.qname() in ("datetime.datetime", "datetime.time"):
+                if isinstance(assigned_value, astroid.Attribute):
+                    if assigned_value.attrname in ("min", "max"):
+                        ass_val_name = assigned_value.attrname
+                        self.naive_properties_methods_replace(
+                            node, ass_val_name, assigned_var
+                        )
+                if isinstance(assigned_value, astroid.Call):
+                    if assigned_value.func.attrname in (
+                        "fromordinal",
+                        "fromisocalendar",
+                    ):
+                        ass_val_name = assigned_value.func.attrname
+                        self.naive_properties_methods_replace(
+                            node, ass_val_name, assigned_var
+                        )
+
+
+def register(linter: pylint.lint.PyLinter) -> None:
+    """Pylint function to register checker.
+
+    Args:
+        linter: instance of pylint linter.
+    """
+    linter.register_checker(DatetimeChecker(linter))
--- tests/test_pylint_datetime.py
+++ tests/test_pylint_datetime.py
@@ -2,7 +2,308 @@
 # Copyright © 2023 Frequenz Energy-as-a-Service GmbH
 
 """Tests for the frequenz.pylint_datetime package."""
+from typing import Any
+
+import astroid  # type: ignore
+import pylint.testutils  # type: ignore
+
+from pylint_datetime import DatetimeChecker
+
+# tz=None and tzinfo=None is always allowed
+
+# it makes a difference for astroid if for example timedelta is called or datetime.timedelta
+# maybe this needs to be tested more thoroughly
 
 
-def test_dummy() -> None:
-    """Test empty function."""
+class TestDatetimeChecker(pylint.testutils.CheckerTestCase):  # type: ignore
+    """Test class for DatetimeChecker."""
+
+    CHECKER_CLASS = DatetimeChecker
+
+    def asserting_messages_calls(
+        self,
+        msg_id: str,
+        node: Any,
+        args: Any = None,
+    ) -> None:
+        """Assert messages for calls.
+
+        Args:
+            msg_id: message id.
+            node: node to check.
+            args: arguments to pass to the message.
+        """
+        with self.assertAddsMessages(
+            pylint.testutils.MessageTest(
+                msg_id=msg_id,
+                node=node,
+                args=args,
+            ),
+            ignore_position=True,
+        ):
+            self.checker.visit_call(node)
+
+    def asserting_messages_assign(
+        self,
+        msg_id: str,
+        node: Any,
+        node2: Any = None,
+        args: Any = None,
+    ) -> None:
+        """Assert messages for assignments.
+
+        Args:
+            msg_id: message id.
+            node: node to check.
+            node2: second node to check.
+            args: arguments to pass to the message.
+        """
+        with self.assertAddsMessages(
+            pylint.testutils.MessageTest(
+                msg_id=msg_id,
+                node=node if node2 is None else node2,
+                args=args,
+            ),
+            ignore_position=True,
+        ):
+            self.checker.visit_assign(node)
+
+    def test_timedelta_without_kargs(self) -> None:
+        """Test timedelta without keyword arguments."""
+        call_node_1, call_node_2 = astroid.extract_node(
+            """
+        import datetime
+        from datetime import timedelta
+        timedelta(1) #@
+        datetime.timedelta(1) #@
+        """
+        )
+        self.asserting_messages_calls("timedelta-no-keyword-args", call_node_1)
+        self.asserting_messages_calls("timedelta-no-keyword-args", call_node_2)
+
+    def test_timedelta_with_kargs(self) -> None:
+        """Test timedelta with keyword arguments."""
+        call_node_1, call_node_2 = astroid.extract_node(
+            """
+        import datetime
+        from datetime import timedelta
+        timedelta(seconds=1) #@
+        datetime.timedelta(seconds=1) #@
+        """
+        )
+        with self.assertNoMessages():
+            self.checker.visit_call(call_node_1)
+            self.checker.visit_call(call_node_2)
+
+    def test_no_timezone_argument(self) -> None:
+        """Test missing timezone arguments."""
+        (
+            call_node_1,
+            call_node_2,
+            call_node_3,
+            call_node_4,
+        ) = astroid.extract_node(
+            """
+        import datetime
+        from datetime import datetime
+        datetime.now() #@
+        datetime.fromtimestamp(1)  #@
+        datetime(1, 1, 1, 1, 1, 1, 1)  #@
+        datetime.astimezone()  #@
+        """
+        )
+        self.asserting_messages_calls(
+            "timezone-no-argument", call_node_1, args=("now",)
+        )
+        self.asserting_messages_calls(
+            "timezone-no-argument", call_node_2, args=("fromtimestamp",)
+        )
+        self.asserting_messages_calls(
+            "timezone-no-argument", call_node_3, args=("datetime",)
+        )
+        self.asserting_messages_calls(
+            "timezone-no-argument", call_node_4, args=("astimezone",)
+        )
+
+    def test_passed_timezone_argument(self) -> None:
+        """Test correctly passed timezone arguments."""
+        (
+            call_node_1,
+            call_node_2,
+            call_node_3,
+            call_node_4,
+        ) = astroid.extract_node(
+            """
+        import datetime
+        from datetime import datetime, timezone
+        datetime.now(tz=timezone.utc) #@
+        datetime.fromtimestamp(1, timezone.utc)  #@
+        datetime(1, 1, 1, 1, 1, 1, 1, timezone.utc)  #@
+        datetime.astimezone(tzinfo=timezone.utc)  #@
+        """
+        )
+        with self.assertNoMessages():
+            self.checker.visit_call(call_node_1)
+            self.checker.visit_call(call_node_2)
+            self.checker.visit_call(call_node_3)
+            self.checker.visit_call(call_node_4)
+
+    def test_no_naive_methods(self) -> None:
+        """Test wrong naive method calls."""
+        (
+            call_node_1,
+            call_node_2,
+            call_node_3,
+            call_node_4,
+            call_node_5,
+        ) = astroid.extract_node(
+            """
+        import datetime
+        from datetime import datetime
+        datetime.today() #@
+        datetime.utcnow() #@
+        datetime.utcfromtimestamp(1)  #@
+        datetime.utctimetuple()  #@
+        datetime.time()  #@
+        """
+        )
+        self.asserting_messages_calls(
+            "naive-datetime-call", call_node_1, args=("today",)
+        )
+        self.asserting_messages_calls(
+            "naive-datetime-call", call_node_2, args=("utcnow",)
+        )
+        self.asserting_messages_calls(
+            "naive-datetime-call", call_node_3, args=("utcfromtimestamp",)
+        )
+        self.asserting_messages_calls(
+            "naive-datetime-call", call_node_4, args=("utctimetuple",)
+        )
+        self.asserting_messages_calls(
+            "naive-datetime-call", call_node_5, args=("time",)
+        )
+
+    def test_naive_objects_not_replaced(self) -> None:
+        """Test naive objects without subsequent call to replace."""
+        (
+            call_node_1,
+            call_node_2,
+            call_node_3,
+            call_node_4,
+            call_node_5,
+            call_node_6,
+        ) = astroid.extract_node(
+            """
+        import datetime
+        dt_min = datetime.datetime.min #@
+        dt_max = datetime.datetime.max #@
+        dt1 = datetime.datetime.fromisocalendar(1, 1, 1) #@
+        dt2 = datetime.datetime.fromordinal(1) #@
+        t_min = datetime.time.min #@
+        t_max = datetime.time.max #@
+        """
+        )
+        self.asserting_messages_assign(
+            "naive-datetime-no-timezone", call_node_1, args=("min",)
+        )
+        self.asserting_messages_assign(
+            "naive-datetime-no-timezone", call_node_2, args=("max",)
+        )
+        self.asserting_messages_assign(
+            "naive-datetime-no-timezone", call_node_3, args=("fromisocalendar",)
+        )
+        self.asserting_messages_assign(
+            "naive-datetime-no-timezone", call_node_4, args=("fromordinal",)
+        )
+        self.asserting_messages_assign(
+            "naive-datetime-no-timezone", call_node_5, args=("min",)
+        )
+        self.asserting_messages_assign(
+            "naive-datetime-no-timezone", call_node_6, args=("max",)
+        )
+
+    def test_naive_objects_falsely_replaced(self) -> None:
+        """Test naive objects with subsequent call to replace but lacking timezone info."""
+        (
+            call_node_1,
+            call_node_2,
+            call_node_3,
+            call_node_4,
+            call_node_5,
+            call_node_6,
+            call_node_7,
+            call_node_8,
+            call_node_9,
+            call_node_10,
+            call_node_11,
+            call_node_12,
+        ) = astroid.extract_node(
+            """
+        import datetime
+        dt_min = datetime.datetime.min #@
+        dt_min.replace(1) #@
+        dt_max = datetime.datetime.max #@
+        dt_max.replace(1) #@
+        dt1 = datetime.datetime.fromisocalendar(1, 1, 1) #@
+        dt1.replace(1) #@
+        dt2 = datetime.datetime.fromordinal(1) #@
+        dt2.replace(year=1) #@
+        t_min = datetime.time.min #@
+        t_min.replace(year=1) #@
+        t_max = datetime.time.max #@
+        t_max.replace(year=1) #@
+
+        """
+        )
+        self.asserting_messages_assign(
+            "timezone-no-argument", call_node_1, call_node_2, args=("min",)
+        )
+        self.asserting_messages_assign(
+            "timezone-no-argument", call_node_3, call_node_4, args=("max",)
+        )
+        self.asserting_messages_assign(
+            "timezone-no-argument", call_node_5, call_node_6, args=("fromisocalendar",)
+        )
+        self.asserting_messages_assign(
+            "timezone-no-argument", call_node_7, call_node_8, args=("fromordinal",)
+        )
+        self.asserting_messages_assign(
+            "timezone-no-argument", call_node_9, call_node_10, args=("min",)
+        )
+        self.asserting_messages_assign(
+            "timezone-no-argument", call_node_11, call_node_12, args=("max",)
+        )
+
+    def test_naive_objects_correctly_replaced(self) -> None:
+        """Test naive objects with subsequent call to replace including timezone info."""
+        (
+            call_node_1,
+            call_node_2,
+            call_node_3,
+            call_node_4,
+            call_node_5,
+            call_node_6,
+        ) = astroid.extract_node(
+            """
+        import datetime
+        dt_min = datetime.datetime.min #@
+        dt_min.replace(tzinfo=1)
+        dt_max = datetime.datetime.max #@
+        dt_max.replace(tzinfo=1)
+        dt1 = datetime.datetime.fromisocalendar(1, 1, 1) #@
+        dt1.replace(tzinfo=1)
+        dt2 = datetime.datetime.fromordinal(1) #@
+        dt2.replace(tzinfo=1)
+        t_min = datetime.time.min #@
+        t_min.replace(tzinfo=1)
+        t_max = datetime.time.max #@
+        t_max.replace(tzinfo=1)
+        """
+        )
+        with self.assertNoMessages():
+            self.checker.visit_assign(call_node_1)
+            self.checker.visit_assign(call_node_2)
+            self.checker.visit_assign(call_node_3)
+            self.checker.visit_assign(call_node_4)
+            self.checker.visit_assign(call_node_5)
+            self.checker.visit_assign(call_node_6)
